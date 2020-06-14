1f23059687c3c875fc21a423a6daa6e692ae436f
1024156741 <lenovo@192.168.228.1>
2020-06-14 17:17:25 +0800

    主干添加

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,11 +3,10 @@ package com.atguigu.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
 		System.out.println("创建分支");
-		
+		System.out.println("主干添加");
 		
 	}
 
